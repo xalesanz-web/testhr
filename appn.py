bc5767615e353e8f43eec0209eca33d3b48cb123
xalesanz-web <xalesanz@gmail.com>
2025-09-22 11:49:13 -0600

    Update appn.py

--- appn.py
+++ appn.py
@@ -14,7 +14,7 @@ st.set_page_config(
 
 # ---- Barra Lateral ----
 with st.sidebar:
-    st.image("IMG/LOGO.jpg", use_container_width=True)
+    st.image("LOGO.jpg", use_container_width=True)
     st.markdown("### Sura Actuarial Desk")
     st.markdown("---")
     
@@ -36,9 +36,10 @@ if 'page' not in st.session_state:
 if st.session_state.page == "inicio":
     col1, col2, col3 = st.columns([1, 4, 1])
     with col2:
-        st.image("IMG/LOGO.jpg", width=400)
+        st.image("LOGO.jpg", width=400)
         st.markdown("<h1 style='text-align: center; color: white;'>Sura Actuarial Desk</h1>", unsafe_allow_html=True)
 # ... [Código para las demás páginas] ...
 
 # ---- Cargar el CSS ----
 load_css("styles.css")
+
